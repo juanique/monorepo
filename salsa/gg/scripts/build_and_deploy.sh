173b798c913becc678e2711ff62210ed916eb5a2
Juan Munoz <juanique@gmail.com>
2021-10-10 17:26:03 -0700

    Remove par support and replace with --build_python_zip

--- salsa/gg/scripts/build_and_deploy.sh
+++ salsa/gg/scripts/build_and_deploy.sh
@@ -1,3 +1,6 @@
-bazel build //gg:gg_cli.par
+bazel build //salsa/gg:gg_cli --build_python_zip
 rm -f ~/bin/gg
-cp bazel-bin/gg/gg_cli.par ~/bin/gg
+
+echo '#!/usr/bin/env python' > ~/bin/gg
+cat bazel-bin/salsa/gg/gg_cli.zip >> ~/bin/gg
+chmod +x ~/bin/gg
