337a6dfc73389e9780422fd1a844b25047affdc8
Juan Munoz <juanique@gmail.com>
2021-10-17 14:00:33 -0700

    Custom py_image rule, clean up greeter_server.

---
+++ bazel/docker.bzl
@@ -0,0 +1,19 @@
+load("@io_bazel_rules_docker//lang:image.bzl", "app_layer")
+load("@io_bazel_rules_docker//container:container.bzl", "container_image")
+
+def py_image(name, binary, port):
+    base_image_name = name + "_base_py_image"
+
+    container_image(
+        name = base_image_name,
+        base = "@ubuntu//image",
+        docker_run_flags = "-p127.0.0.1:" + port + ":" + port,
+    )
+
+    app_layer(
+        name = name,
+        base = ":" + base_image_name,
+        binary = ":greeter_server",
+        create_empty_workspace_dir = True,
+        entrypoint = ["/usr/bin/python3.10"],
+    )
--- examples/grpc/server/greeter_server.py
+++ examples/grpc/server/greeter_server.py
@@ -5,25 +5,29 @@ import grpc
 import logging
 import sys
 
-print(helloworld_pb2)
-print(repr(sys.version_info))
-
 
 class Greeter(helloworld_pb2_grpc.GreeterServicer):
     def SayHello(self, req, context):
         logging.info("Got request %s", req)
-        print("=> Got request %s", req)
         return helloworld_pb2.HelloReply(message="Hello %s!" % req.name)
 
 
 def serve():
+    port = 50051
+    logging.info(f"Server starting at port {port}")
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     helloworld_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
-    server.add_insecure_port("[::]:50051")
+    server.add_insecure_port(f"[::]:{port}")
     server.start()
     server.wait_for_termination()
 
 
 if __name__ == "__main__":
-    logging.basicConfig()
+    logging.basicConfig(
+        stream=sys.stdout,
+        filemode="w",
+        format="%(levelname)s %(asctime)s - %(message)s",
+        level=logging.INFO,
+    )
+
     serve()
