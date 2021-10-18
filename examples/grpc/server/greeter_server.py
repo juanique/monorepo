df9ff42861ccc986f196b8b5a57c405d464947fc
Juan Munoz <juanique@gmail.com>
2021-10-17 22:33:21 -0700

    Add reflection to rpc server

--- examples/grpc/server/greeter_server.py
+++ examples/grpc/server/greeter_server.py
@@ -1,6 +1,7 @@
 from concurrent import futures
 from examples.grpc.protos import helloworld_pb2
 from examples.grpc.protos import helloworld_pb2_grpc
+from grpc_reflection.v1alpha import reflection
 import grpc
 import logging
 import sys
@@ -17,6 +18,11 @@ def serve():
     logging.info(f"Server starting at port {port}")
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     helloworld_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
+    SERVICE_NAMES = (
+        helloworld_pb2.DESCRIPTOR.services_by_name["Greeter"].full_name,
+        reflection.SERVICE_NAME,
+    )
+    reflection.enable_server_reflection(SERVICE_NAMES, server)
     server.add_insecure_port(f"[::]:{port}")
     server.start()
     server.wait_for_termination()
