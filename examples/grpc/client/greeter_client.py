63b4d9939d817644c3522955bd7f4ea65d1e81a5
Juan Munoz <juanique@gmail.com>
2021-10-25 22:30:37 -0700

    Add integration test for greeter client

--- examples/grpc/client/greeter_client.py
+++ examples/grpc/client/greeter_client.py
@@ -1,24 +1,15 @@
-from __future__ import print_function
-
-import logging
-import sys
-
 import grpc
 from examples.grpc.protos import helloworld_pb2
 from examples.grpc.protos import helloworld_pb2_grpc
 
 
-def run(name):
-    # NOTE(gRPC Python Team): .close() is possible on a channel and should be
-    # used in circumstances in which the with statement does not fit the needs
-    # of the code.
-    with grpc.insecure_channel("localhost:50051") as channel:
-        stub = helloworld_pb2_grpc.GreeterStub(channel)
-        response = stub.SayHello(helloworld_pb2.HelloRequest(name=name))
-    print("Greeter client received: " + response.message)
+class GreeterClient:
+    def __init__(self, address: str):
+        self.address = address
+        channel = grpc.insecure_channel(self.address)
+        grpc.channel_ready_future(channel).result(timeout=20)
+        self.stub = helloworld_pb2_grpc.GreeterStub(channel)
 
-
-if __name__ == "__main__":
-    logging.basicConfig()
-    name = sys.argv[1] if len(sys.argv) > 1 else "there"
-    run(name)
+    def say_hello(self, name: str) -> str:
+        response = self.stub.SayHello(helloworld_pb2.HelloRequest(name=name))
+        return response.message
---
+++ examples/grpc/client/greeter_client_integration_test.py
@@ -0,0 +1,30 @@
+import unittest
+import docker
+
+from salsa.docker.testing.docker_service import DockerService
+from examples.grpc.client.greeter_client import GreeterClient
+
+
+class TestGreeterClient(unittest.TestCase):
+    def setUp(self) -> None:
+        self.docker_client = docker.from_env()
+        self.service = DockerService(
+            self.docker_client,
+            image_name="bazel/examples/grpc/server",
+            image_tar="monorepo/examples/grpc/server/greeter_server_docker.tar",
+            container_name="greeter_server",
+            port=50051,
+        )
+        self.service.start()
+
+    def tearDown(self) -> None:
+        self.service.stop()
+        self.docker_client.close()
+
+    def test_hello(self) -> None:
+        client = GreeterClient(self.service.hostport())
+        self.assertEqual("Hello Juan!", client.say_hello("Juan"))
+
+
+if __name__ == "__main__":
+    unittest.main()
