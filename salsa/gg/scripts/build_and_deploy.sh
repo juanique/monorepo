66efbea8956f17357dce86f11592322528e1c832
Juan Munoz <juanique@gmail.com>
2022-06-30 04:42:03 +0000

    Fix finding git repo root

--- salsa/gg/scripts/build_and_deploy.sh
+++ salsa/gg/scripts/build_and_deploy.sh
@@ -7,6 +7,6 @@ fi
 bzl build //salsa/gg:gg_cli --build_python_zip
 rm -f ~/bin/gg
 
-echo '#!/usr/bin/env python' > ~/bin/gg
-cat bazel-bin/salsa/gg/gg_cli.zip >> ~/bin/gg
+echo '#!/usr/bin/env python' > ~/bin/gg_dev
+cat bazel-bin/salsa/gg/gg_cli.zip >> ~/bin/gg_dev
 chmod +x ~/bin/gg
