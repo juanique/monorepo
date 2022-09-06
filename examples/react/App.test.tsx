caefd6d0c466faa7428ba6acac0b67b14aeeff47
Juan Munoz <juan.munoz@verkada.com>
2022-09-06 19:32:00 +0000

    Add basic react example.

---
+++ examples/react/App.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders hello world', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Hello, world!/i);
+  expect(linkElement).toBeInTheDocument();
+});
