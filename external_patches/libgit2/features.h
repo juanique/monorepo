2a336bad2e74986a78e0211b9ae5498e68204312
Juan Munoz <juanique@gmail.com>
2023-08-27 07:37:52 -0700

    Snapshot #7

--- external_patches/libgit2/features.h
+++ external_patches/libgit2/features.h
@@ -1,3 +1,5 @@
+// For libgit2
+
 #ifndef INCLUDE_features_h__
 #define INCLUDE_features_h__
 
@@ -35,66 +37,5 @@
 #undef GIT_SHA1_WIN32
 #undef GIT_SHA1_COMMON_CRYPTO
 #undef GIT_SHA1_OPENSSL
-
-#endif
-
-
-#ifndef INCLUDE_features_h__
-#define INCLUDE_features_h__
-
-// PCRE
-#define HAVE_DIRENT_H 1
-#define HAVE_SYS_STAT_H 1
-#define HAVE_SYS_TYPES_H 1
-#define HAVE_UNISTD_H 1
-#define HAVE_WINDOWS_H 1
-#define HAVE_STDINT_H 1
-#define HAVE_INTTYPES_H 1
-
-#define HAVE_TYPE_TRAITS_H 1
-#define HAVE_BITS_TYPE_TRAITS_H 1
-
-#define HAVE_BCOPY 1
-#define HAVE_MEMMOVE 1
-#define HAVE_STRERROR 1
-#define HAVE_STRTOLL 1
-#define HAVE_STRTOQ 1
-#define HAVE__STRTOI64 1
-
-#define PCRE_STATIC 1
-
-#define SUPPORT_PCRE8 1
-#define SUPPORT_PCRE16 1
-#define SUPPORT_PCRE32 1
-#define SUPPORT_JIT 1
-#define SUPPORT_PCREGREP_JIT 1
-#define SUPPORT_UTF 1
-#define SUPPORT_UCP 1
-#define EBCDIC 1
-#define EBCDIC_NL25 1
-#define BSR_ANYCRLF 1
-#define NO_RECURSE 1
-
-#define HAVE_LONG_LONG 1
-#define HAVE_UNSIGNED_LONG_LONG 1
-
-#define SUPPORT_LIBBZ2 1
-#define SUPPORT_LIBZ 1
-#define SUPPORT_LIBEDIT 1
-#define SUPPORT_LIBREADLINE 1
-
-#define SUPPORT_VALGRIND 1
-#define SUPPORT_GCOV 1
-
-#define NEWLINE                 @NEWLINE@
-#define POSIX_MALLOC_THRESHOLD  @PCRE_POSIX_MALLOC_THRESHOLD@
-#define LINK_SIZE               @PCRE_LINK_SIZE@
-#define PARENS_NEST_LIMIT       @PCRE_PARENS_NEST_LIMIT@
-#define MATCH_LIMIT             @PCRE_MATCH_LIMIT@
-#define MATCH_LIMIT_RECURSION   @PCRE_MATCH_LIMIT_RECURSION@
-#define PCREGREP_BUFSIZE        @PCREGREP_BUFSIZE@
-
-#define MAX_NAME_SIZE   32
-#define MAX_NAME_COUNT  10000
 
 #endif
