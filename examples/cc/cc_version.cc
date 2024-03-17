5fc2a4fc05d20d3b1cac9138dc00ecfdb8c66d21
juanique <juanique@gmail.com>
2024-03-17 14:26:11 -0700

    Add support for rules_apko and rules_debian_packages (#155)

--- examples/cc/cc_version.cc
+++ examples/cc/cc_version.cc
@@ -1,4 +1,19 @@
 #include<iostream>
+#include <stdio.h>
+#if defined(_WIN64)
+#include <windows.h>
+#define OS "windows"
+#elif __APPLE__
+#define OS "macos"
+#elif __linux__
+#include <features.h>
+#define OS "linux"
+#elif __wasi__
+#include <features.h>
+#define OS "wasi"
+#else
+#   error "Unknown compiler!"
+#endif
 
 int main() {
     if (__cplusplus == 202101L) std::cout << "C++23";
@@ -9,4 +24,22 @@ int main() {
     else if (__cplusplus == 199711L) std::cout << "C++98";
     else std::cout << "pre-standard C++." << __cplusplus;
     std::cout << "\n";
+
+#if defined(_WIN64)
+    DWORD version = GetVersion();
+    DWORD majorVersion = (DWORD)(LOBYTE(LOWORD(version)));
+    DWORD minorVersion = (DWORD)(HIBYTE(LOWORD(version)));
+
+    DWORD build = 0;
+    if (version < 0x80000000) {
+        build = (DWORD)(HIWORD(version));
+    }
+
+    printf("%s %lu.%lu (%lu).\n", OS, majorVersion, minorVersion, build);
+#elif defined __GLIBC__
+    printf("%s glibc_%d.%d\n", OS, __GLIBC__, __GLIBC_MINOR__);
+#else
+    printf("%s non-glibc\n", OS);
+#endif
+    return 0;
 }
