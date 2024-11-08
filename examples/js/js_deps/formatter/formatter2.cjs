037e86dd06ef2fa30e3ef5e03f04a95cc177fb24
Juan Munoz <juan.munoz@verkada.com>
2024-11-08 10:02:58 -0800

    add a cjs alternative

---
+++ examples/js/js_deps/formatter/formatter2.cjs
@@ -0,0 +1,12 @@
+function prettyFormatName(name) {
+    if (!name || typeof name !== 'string') {
+        throw new Error("Invalid name input. Please provide a non-empty string.");
+    }
+    return name
+        .toLowerCase()
+        .split(' ')
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(' ');
+}
+
+module.exports = { prettyFormatName };
---
+++ examples/js/js_deps/main2.cjs
@@ -0,0 +1,4 @@
+const { prettyFormatName } = require("examples/js/js_deps/formatter/formatter2.cjs");
+
+console.log("process.env.NODE_PATH", process.env.NODE_PATH)
+console.log(prettyFormatName("JUAN MUNOZ"))
