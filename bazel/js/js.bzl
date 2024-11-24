93a732477868e89666bc3bf9d97601751d5012d1
Juan Munoz <juanique@gmail.com>
2024-11-24 01:26:29 -0800

    Snapshot #4

--- bazel/js/js.bzl
+++ bazel/js/js.bzl
@@ -15,3 +15,32 @@ def js_binary(data = [], node_options = [], **kwargs):
 
 js_library = _js_library
 js_run_devserver = _js_run_devserver
+
+def _vite_build_impl(ctx):
+    output_dir = ctx.actions.declare_directory(ctx.attr.name)
+    script = """!
+{tool} build --config {config}
+""".format(tool = ctx.executable.tool.path, config = ctx.file.config.path)
+    ctx.actions.run_shell(
+        inputs = [ctx.executable.tool, ctx.file.config] + ctx.files.data,
+        outputs = [output_dir],
+        command = script,
+    )
+
+    return [DefaultInfo(files = depset([output_dir]))]
+
+
+
+vite_build = rule(
+    implementation = _vite_build_impl,
+    attrs = {
+        "data": attr.label_list(allow_files = True),
+        "config": attr.label(allow_single_file = True),
+        "tool": attr.label(
+            allow_files = True,
+            default = "//:vite.build",
+            executable = True,
+            cfg = "exec",
+        ),
+    },
+)
--- examples/webapp/vite.config.js
+++ examples/webapp/vite.config.js
@@ -1,10 +1,77 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+
+import fs from 'fs/promises'; // Import the file system module
+import { hasUncaughtExceptionCaptureCallback } from 'process';
+
+console.log('***********************************');
+const projectRoot = process.cwd(); // Current working directory
+console.log('Vite Project Root:', projectRoot);
+
 
 export default defineConfig({
-    plugins: [react()],
-    server: {},
-    build: {
-        outDir: 'dist',
+    root: '.',
+    server: {
+        port: 3000, // Specify the port number
     },
+    build: {
+        outDir: './dist', // Specify the output directory for the build
+        rollupOptions: {
+            plugins: [
+                {
+                    name: 'copy-html-plugin',
+                    buildStart: async () => {
+                        try {
+                            // 1. Read the index.html file
+                            const htmlContent = await fs.readFile('./examples/webapp/index.html', 'utf-8');
+
+                            // 2. Write the file to the destination (project root)
+                            await fs.writeFile('./index.html', htmlContent);
+                            console.log('index.html copied to vite project root');
+                            // await fs.writeFile('./dist/index.html', htmlContent);
+                        } catch (err) {
+                            console.error('Error copying index.html:', err);
+                        }
+                    },
+                    writeBundle: async () => {
+                        // List all files in the dist directory, recursively
+
+                        // Move ./dist to examples/webapp/dist
+                        await fs.rename('./dist', 'examples/webapp/dist');
+                        // create dist directory
+                        // await fs.mkdir('examples/webapp/dist', { recursive: true });
+                        // write something to it
+                        // await fs.writeFile('examples/webapp/dist/test.txt', 'test');
+                        console.log('Current working directory is :', process.cwd());
+                        // console.log("pp")
+
+                        const files = await fs.readdir('./', { recursive: true });
+                        for (const file of files) {
+                            // If contains dist, log the file and not node_modules
+                            if (file.includes('dist') && !file.includes('node_modules')) {
+                                console.log(file);
+                            }
+                        }
+                        // throw new Error('buildEnd');
+                    },
+                },
+            ],
+        },
+    },
+
+    plugins: [
+        react(),
+        {
+            name: 'redirect-root',
+            configureServer(server) {
+                server.middlewares.use((req, res, next) => {
+                    if (req.url === '/') {
+                        req.url = '/examples/webapp/index.html'; // Redirect to the desired file
+                    }
+                    next();
+                });
+            },
+        },
+    ],
 });
