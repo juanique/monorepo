ce38f129365ab3aee9390c52197573384e894b3c
Juan Munoz <juan.munoz@verkada.com>
2024-12-30 10:13:48 -0800

    Add a playwright test using ts sources

---
+++ examples/playwright/ts_sources/playwright.config.js
@@ -0,0 +1,55 @@
+import { defineConfig, devices } from '@playwright/test';
+
+async function createConfig() {
+
+    process.env.PLAYWRIGHT_HTML_OUTPUT_DIR = process.env.TEST_UNDECLARED_OUTPUTS_DIR + "/html";
+    process.env.PLAYWRIGHT_JUNIT_OUTPUT_NAME = process.env.XML_OUTPUT_FILE;
+
+    return defineConfig({
+        // Look for test files in the "tests" directory, relative to this configuration file.
+        testDir: '.',
+
+        // Run all tests in parallel.
+        fullyParallel: true,
+
+        // Fail the build on CI if you accidentally left test.only in the source code.
+        forbidOnly: !!process.env.CI,
+
+        use: {
+            trace: 'retain-on-failure',
+        },
+
+        contextOptions: {
+            recordVideo: {
+                dir: process.env.TEST_UNDECLARED_OUTPUTS_DIR + "/videos",
+                size: { width: 640, height: 480 },
+            }
+        },
+
+        // Reporter to use
+        reporter: [
+            ['list', { printSteps: true }],
+            ['junit'],
+            ["html"],
+        ],
+
+        outputDir: process.env.TEST_UNDECLARED_OUTPUTS_DIR + "/reports",
+
+        // Configure projects for major browsers.
+        projects: [
+            {
+                name: 'chromium',
+                use: {
+                    ...devices['Desktop Chrome'],
+                    launchOptions: {
+                        // When in CI, use bazel packaged linux chromium
+                        executablePath: process.env.CHROMIUM_BIN,
+                    },
+                    viewport: { width: 640, height: 480 },
+                },
+            },
+        ],
+    });
+}
+
+export default createConfig();
--- third_party/binaries/repositories.bzl
+++ third_party/binaries/repositories.bzl
@@ -1,5 +1,15 @@
 load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
 
+NATIVE_BINARY_BUILD_TPL = """
+load("@bazel_skylib//rules:native_binary.bzl", "native_binary")
+package(default_visibility = ["//visibility:public"])
+native_binary(
+    name = "{name}",
+    src = "{binary}",
+    out = "{binary}",
+)
+"""
+
 def binary_repositories():
     http_archive(
         name = "docker",
@@ -7,4 +17,24 @@ def binary_repositories():
         strip_prefix = "docker",
         url = "https://download.docker.com/linux/static/stable/x86_64/docker-25.0.4.tgz",
         build_file = "//third_party/binaries:BUILD.docker.bazel",
+    )
+
+    http_archive(
+        name = "chromium_linux_amd64",
+        url = "https://playwright.azureedge.net/builds/chromium/1129/chromium-linux.zip",
+        sha256 = "e6091f35f81cfbdf4fd3eb4f3f7916e841d43582f113f39f6e0d0c253485e56a",
+        build_file_content = NATIVE_BINARY_BUILD_TPL.format(
+            name = "chromium-bin",
+            binary = "chrome-linux/chrome",
+        ),
+    )
+
+    http_archive(
+        name = "chromium_macos_arm64",
+        url = "https://playwright.azureedge.net/builds/chromium/1129/chromium-mac-arm64.zip",
+        sha256 = "59d179d0ad9ad792e51db81aef2d7bcc164e6f5df434c081888fbbeb4568bd0e",
+        build_file_content = NATIVE_BINARY_BUILD_TPL.format(
+            name = "chromium-bin",
+            binary = "chrome-mac/Chromium.app/Contents/MacOS/Chromium",
+        ),
     )
